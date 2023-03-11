ba89823bdbb0549c43bc814af2ff64efde7ab8d1
girnal82 <grinal82@gmail.com>
2023-03-11 13:06:08 +0300

    pop up disappear on second click added

--- tooltip/task.js
+++ tooltip/task.js
@@ -1,37 +1,24 @@
-// NodeList of elements having ".has-tooltip" class
-const hasTooltipElements = document.querySelectorAll(".has-tooltip");
-// a variable to track if there's an active tooltip element
-let activeTooltip = null;
+const tooltips = document.querySelectorAll(".has-tooltip");
 
-// a loop over NodeList to add eventlistener to each element
-hasTooltipElements.forEach((element) => {
-    element.addEventListener("click", (event) => {
-        event.preventDefault();
-        const tooltipText = element.getAttribute("title");
-        const tooltipElement = document.createElement("div");
-        tooltipElement.classList.add("tooltip");
-        tooltipElement.textContent = tooltipText;
-        tooltipElement.style.top =
-            element.offsetTop + element.offsetHeight + "px";
-        tooltipElement.style.left = element.offsetLeft + "px";
+tooltips.forEach((tooltip) => {
+    tooltip.addEventListener("click", (e) => {
+        e.preventDefault();
 
-        // If the tooltip for the clicked element is already active
-        if (activeTooltip && activeTooltip === tooltipElement) {
-            tooltipElement.classList.remove("tooltip_active"); // hide the tooltip
-            activeTooltip = null; // reset the activeTooltip variable
-        } else {
-            // If the clicked element doesn't have an active tooltip
+        const tooltipText = tooltip.getAttribute("title");
+        const tooltipActive = document.querySelector(".tooltip_active");
 
-            // If there is already an active tooltip
-            if (activeTooltip) {
-                activeTooltip.classList.remove("tooltip_active"); // hide the active tooltip
-            }
-            element.parentNode.insertBefore(
-                tooltipElement,
-                element.nextSibling
-            );
-            activeTooltip = tooltipElement; // set the activeTooltip variable to the new tooltip
-            tooltipElement.classList.add("tooltip_active"); // show the tooltip
+        if (tooltipActive && tooltipActive.textContent === tooltipText) {
+            tooltipActive.classList.remove("tooltip_active");
+            return;
         }
+
+        if (tooltipActive) {
+            tooltipActive.classList.remove("tooltip_active");
+        }
+
+        const tooltipDiv = document.createElement("div");
+        tooltipDiv.classList.add("tooltip", "tooltip_active");
+        tooltipDiv.textContent = tooltipText;
+        tooltip.parentNode.insertBefore(tooltipDiv, tooltip.nextSibling);
     });
 });
